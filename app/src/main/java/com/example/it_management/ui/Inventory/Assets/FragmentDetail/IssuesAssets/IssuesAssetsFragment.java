219ddd7e1d8ee5a841f86697f1240b46bc4ce20c
Deo Adzar <61385321+DeoAdzar@users.noreply.github.com>
2021-07-16 09:54:42 +0700

    16 july 2021 9:54 AM

--- app/src/main/java/com/example/it_management/ui/Inventory/Assets/FragmentDetail/IssuesAssets/IssuesAssetsFragment.java
+++ app/src/main/java/com/example/it_management/ui/Inventory/Assets/FragmentDetail/IssuesAssets/IssuesAssetsFragment.java
@@ -1,7 +1,9 @@
 package com.example.it_management.ui.Inventory.Assets.FragmentDetail.IssuesAssets;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -15,8 +17,10 @@ import android.widget.Toast;
 import com.example.it_management.API.BaseApiService;
 import com.example.it_management.API.UtilsApi;
 import com.example.it_management.R;
+import com.example.it_management.ui.AddActivity.AddIssues;
 import com.example.it_management.ui.Clients.FragmentDetail.ClientIssues.ClientsIssuesAdapterData;
 import com.example.it_management.ui.Clients.FragmentDetail.ClientIssues.ClientsIssuesModel;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -33,6 +37,7 @@ public class IssuesAssetsFragment extends Fragment {
     private TextView noData;
     private List<AssetIssuesModel> assetIssuesModelList = new ArrayList<>();
     private int id;
+    FloatingActionButton fabaddAssetIssues;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -41,13 +46,44 @@ public class IssuesAssetsFragment extends Fragment {
         rvIssuesAsset = v.findViewById(R.id.rv_asset_issues);
         lmIssuesAsset = new LinearLayoutManager(getContext(),RecyclerView.VERTICAL,false);
         rvIssuesAsset.setLayoutManager(lmIssuesAsset);
+        fabaddAssetIssues = v.findViewById(R.id.fab_add_issues_asset);
         id = Integer.parseInt(getArguments().getString("idAssets"));
         noData = v.findViewById(R.id.no_data_issues_asset);
         noData.setVisibility(View.GONE);
         TampilDataIssues();
+        addIssues();
+        hilang();
         return v;
     }
+    @Override
+    public void onResume() {
+        super.onResume();
+        TampilDataIssues();
+    }
 
+    private void hilang(){
+        rvIssuesAsset.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                if (dy > 0 && fabaddAssetIssues.getVisibility() == View.VISIBLE) {
+                    fabaddAssetIssues.hide();
+                } else if (dy < 0 && fabaddAssetIssues.getVisibility() != View.VISIBLE) {
+                    fabaddAssetIssues.show();
+                }
+            }
+        });
+    }
+    private void addIssues(){
+        fabaddAssetIssues.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity(), AddIssues.class);
+                startActivity(i);
+                onStop();
+            }
+        });
+    }
     private void TampilDataIssues() {
         BaseApiService mApiService = UtilsApi.getApiService();
         Call<AssetIssuesResponseModel> tampil = mApiService.basGetIssuesAssets(id);
